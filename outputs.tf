1c10f03e5fa0099ff2fae5fd129fe1703e88d6af
Nathan Parraga Thiesen <nathan.thiesen@mineiros.io>
2022-03-07 15:11:28 +0100

    feat: initial module implementation

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,21 @@
 # OUTPUT CALCULATED VARIABLES (prefer full objects)
 # ----------------------------------------------------------------------------------------------------------------------
 
+ocals {
+  binding = try(google_compute_subnetwork_iam_binding.binding[0], null)
+  member  = try(google_compute_subnetwork_iam_member.member, null)
+  policy  = try(google_compute_subnetwork_iam_policy.policy[0], null)
+
+  iam_output = [local.binding, local.member, local.policy]
+
+  iam_output_index = var.policy_bindings != null ? 2 : var.authoritative ? 0 : 1
+}
+
+output "iam" {
+  description = "All attributes of the created 'iam_binding' or 'iam_member' or 'iam_policy' resource according to the mode."
+  value       = local.iam_output[local.iam_output_index]
+}
+
 # ----------------------------------------------------------------------------------------------------------------------
 # OUTPUT ALL RESOURCES AS FULL OBJECTS
 # ----------------------------------------------------------------------------------------------------------------------
@@ -14,8 +29,3 @@ output "module_enabled" {
   description = "Whether or not the module is enabled."
   value       = var.module_enabled
 }
-
-# output "module_tags" {
-#   description = "A map of tags that will be applied to all created resources that accept tags."
-#   value       = var.module_tags
-# }
--- variables.tf
+++ variables.tf
@@ -3,10 +3,44 @@
 # These variables must be set when using this module.
 # ----------------------------------------------------------------------------------------------------------------------
 
+variable "subnetwork" {
+  description = "(Required) Used to find the parent resource to bind the IAM policy to"
+  type        = string
+}
+
 # ----------------------------------------------------------------------------------------------------------------------
 # OPTIONAL PARAMETERS
 # These variables have defaults, but may be overridden.
 # ----------------------------------------------------------------------------------------------------------------------
+
+variable "members" {
+  type        = set(string)
+  description = "(Optional) Identities that will be granted the privilege in role. Each entry can have one of the following values: 'allUsers', 'allAuthenticatedUsers', 'user:{emailid}', 'serviceAccount:{emailid}', 'group:{emailid}', 'domain:{domain}', 'projectOwner:projectid', 'projectEditor:projectid', 'projectViewer:projectid'."
+  default     = []
+
+  validation {
+    condition     = alltrue([for m in var.members : can(regex("^(allUsers|allAuthenticatedUsers|(user|serviceAccount|group|domain|projectOwner|projectEditor|projectViewer):)", m))])
+    error_message = "The value must be a non-empty list of strings where each entry is a valid principal type identified with `allUsers`, `allAuthenticatedUsers` or prefixed with `user:`, `serviceAccount:`, `group:`, `domain:`, `projectOwner:`, `projectEditor:` or `projectViewer:`."
+  }
+}
+
+variable "role" {
+  description = "(Optional) The role that should be applied. Only one 'iam_binding' can be used per role. Note that custom roles must be of the format '[projects|organizations]/{parent-name}/roles/{role-name}'."
+  type        = string
+  default     = null
+}
+
+variable "authoritative" {
+  description = "(Optional) Whether to exclusively set (authoritative mode) or add (non-authoritative/additive mode) members to the role."
+  type        = bool
+  default     = true
+}
+
+variable "policy_bindings" {
+  description = "(Optional) A list of IAM policy bindings."
+  type        = any
+  default     = null
+}
 
 # ----------------------------------------------------------------------------------------------------------------------
 # MODULE CONFIGURATION PARAMETERS
@@ -18,18 +52,6 @@ variable "module_enabled" {
   description = "(Optional) Whether or not to create resources within the module."
   default     = true
 }
-
-# variable "module_tags" {
-#   type        = map(string)
-#   description = "(Optional) A map of tags that will be applied to all created resources that accept tags. Tags defined with 'module_tags' can be overwritten by resource-specific tags."
-#   default     = {}
-# }
-
-# variable "module_timeouts" {
-#   description = "(Optional) A map of timeout objects that is keyed by Terraform resource name defining timeouts for `create`, `update` and `delete` Terraform operations."
-#   type        = any
-#   default     = null
-# }
 
 variable "module_depends_on" {
   type        = any
